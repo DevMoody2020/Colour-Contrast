16dafcc76f478aac5ae3a76923f377b2cb622d1e
DevMoody2020 <99883349+DevMoody2020@users.noreply.github.com>
2022-03-02 20:29:30 +0000

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,31 @@
+const { EleventyServerlessBundlerPlugin } = require('@11ty/eleventy');
+
+// ewww! (forces the dependency to be included for serverless)
+require('get-contrast');
+
+/**
+ * @typedef {import('@11ty/eleventy/src/UserConfig')} EleventyConfig
+ * @typedef {ReturnType<import('@11ty/eleventy/src/defaultConfig')>} EleventyReturnValue
+ * @type {(eleventyConfig: EleventyConfig)) => EleventyReturnValue}
+ */
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addWatchTarget('src/css');
+
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    functionsDir: 'netlify/functions',
+    name: 'onrequest',
+  });
+
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    functionsDir: 'netlify/functions',
+    name: 'cached',
+    redirects: 'netlify-toml-builders',
+  });
+
+  return {
+    dir: {
+      input: 'src',
+    },
+  };
+};
